6fcbfa2af4339835a542818968dd275a901ce3ae
Eric Shen <89145667+erkshen@users.noreply.github.com>
2025-02-27 18:40:06 +1000

    Allow multi-row submission

--- script.js
+++ script.js
@@ -14,48 +14,50 @@ async function generateWord() {
         }
         
         // Parse Excel data
-        const rowData = parseExcelData(excelDataText);
+        const rowsData = parseExcelData(excelDataText);
         
         // Load the template document
         const templateUrl = 'High Risk Manoeuvre Template.docx';
         
         const docxType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
-        PizZipUtils.getBinaryContent(
-            templateUrl,
-            function (error, content) {
-                if (error) {
-                    console.error(error);
-                    return;
-                }
 
-                const zip = new PizZip(content);
-                const doc = new docxtemplater(zip);
-
-                doc.render({
-                    description: rowData[2],
-                    entry_time: rowData[3],
-                    entry_gate: rowData[4],
-                    exit_time: rowData[5],
-                    exit_gate: rowData[6],
-                    fee: rowData[8],
-                    paid_amt: '$0',
-                    serial: rowData[9] || 'No',
-                    author: rowData[15],
-                    offender_name: rowData[10],
-                    offender_contact: rowData[11],
-                    offender_vehicle: rowData[12],
-                    offender_store: rowData[13],
-                });
-                
-                const out = doc.getZip().generate({
-                    type: "blob",
-                    mimeType: docxType,
-                });
-                saveAs(out, `${rowData[1]}.docx`);
-                
-            }
-        );
+        rowsData.forEach(function (rowData, index) {
+            PizZipUtils.getBinaryContent(
+                templateUrl,
+                function (error, content) {
+                    if (error) {
+                        console.error(error);
+                        return;
+                    }
         
+                    const zip = new PizZip(content);
+                    const doc = new docxtemplater(zip);
+        
+                    doc.render({
+                        description: rowData[2],
+                        entry_time: rowData[3],
+                        entry_gate: rowData[4],
+                        exit_time: rowData[5],
+                        exit_gate: rowData[6],
+                        fee: rowData[8],
+                        paid_amt: '$0',
+                        serial: rowData[9] || 'No',
+                        author: rowData[15],
+                        offender_name: rowData[10],
+                        offender_contact: rowData[11],
+                        offender_vehicle: rowData[12],
+                        offender_store: rowData[13],
+                    });
+                    
+                    const out = doc.getZip().generate({
+                        type: "blob",
+                        mimeType: docxType,
+                    });
+                    saveAs(out, `${rowData[1]}.docx`);
+                    
+                }
+            );
+        });
     } catch (error) {
         console.error('Error generating document:', error);
         alert('Error generating document: ' + error.message);
@@ -107,9 +109,13 @@ function loadFile(url) {
 }
 
 function parseExcelData(excelText) {
-    const rows = excelText.split('\t').map(function(item) {
-        return item.trim();
+	// loop through rows to get individual cells in each row
+    const rows = excelText.split('\n').map(function(row) {
+        return row.split('\t').map(function(cell) {
+        	return cell.trim();
+        });
     });
+    
     console.log(rows);
     return rows;
 }
