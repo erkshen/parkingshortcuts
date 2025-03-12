23e4742ef843a5a07635208fb97da886ca6c8f54
Eric Shen <89145667+erkshen@users.noreply.github.com>
2025-03-12 15:12:06 +1000

    Update script.js

--- AutoreadParkingLogic/script.js
+++ AutoreadParkingLogic/script.js
@@ -1,13 +1,18 @@
 // script.js - With Image Module Support
-async function read_text() {
-	const worker = await Tesseract.createWorker('eng');
-	const previewImage = document.getElementById('previewImage');
 
-	if (previewImage !== undefined) {
-		(async () => {
-		  const { data: { text } } = await worker.recognize(previewImage.src);
-		  console.log(text);
-		  await worker.terminate();
-		})();
-	};
+async function read_text() {
+    const worker = await Tesseract.createWorker('eng');
+    const previewImage = document.getElementById('previewImage');
+    /*await worker.setParameters({
+        tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
+    });
+    */
+
+    if (previewImage) {
+        (async () => {
+            const { data: { text } } = await worker.recognize(previewImage.src);
+            console.log(text);
+            await worker.terminate();
+        })();
+    }
 }
